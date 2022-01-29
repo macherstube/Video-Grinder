c4a7f23b5fca57bdf949155cbfd782bad83a67e7
Josias Bruderer <josias@macherstube.ch>
2022-01-29 18:05:24 +0100

    mailer error handling

--- modules/mailer.py
+++ modules/mailer.py
@@ -10,6 +10,7 @@
 
 import smtplib
 import ssl
+import logging
 from datetime import datetime
 from email import utils
 
@@ -23,14 +24,19 @@ class Mailer:
     def send(self, subject, message):
         if self.config["hostname"] != "":
             context = ssl.create_default_context()
-            with smtplib.SMTP_SSL(self.config["hostname"], self.config["port"],
-                                  context=context) as server:
-                server.set_debuglevel(1)
-                server.login(self.config["username"], self.config["password"])
-                server.sendmail(self.config["username"],
-                                self.config["receiver"],
-                                self.prepare_message(subject, message))
-                server.close()
+            mailcontent = self.prepare_message(subject, message)
+            try:
+                with smtplib.SMTP_SSL(self.config["hostname"], self.config["port"],
+                                      context=context) as server:
+                    server.set_debuglevel(1)
+                    server.login(self.config["username"], self.config["password"])
+                    server.sendmail(self.config["username"],
+                                    self.config["receiver"],
+                                    mailcontent)
+                    server.close()
+            except Exception as e:
+                logging.error("Failed to send mail via smtp: " + str(e))
+                logging.info(mailcontent)
 
     def prepare_message(self, subject, body):
         message_structure = 'From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s' % \
